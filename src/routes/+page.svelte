aaea4d0b8562b449c7fde89b5263a452b61b7d65
Erik Badger <erikbadger777@gmail.com>
2024-12-29 14:08:21 -0600

    fix: header navigation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,18 +5,18 @@
 <div class="flex flex-col gap-20 lg:flex-row p-10 lg:p-0 lg:pl-20 lg:pr-20 ">
   <Header />
   <div class="flex flex-col gap-20 lg:w-1/2 lg:pt-20 lg:pb-20">
-    <h2 id="about" class="sticky top-0 bg-shark">About</h2>
-    <div class="flex flex-col gap-5">
+    <h2 class="sticky top-0 bg-shark">About</h2>
+    <div id="about" class="flex flex-col gap-5">
       {@html about}
     </div>
-    <h2 id="experience" class="sticky top-0 bg-shark">Experience</h2>
-    <div class="flex flex-col gap-10">
+    <h2 class="sticky top-0 bg-shark">Experience</h2>
+    <div id="experience" class="flex flex-col gap-10">
       {#each data.experience as experience}
         <Experience md={experience} />
       {/each}
     </div>
-    <h2 id="projects" class="sticky top-0 bg-shark">Projects</h2>
-    <div class="flex flex-col gap-10">
+    <h2 class="sticky top-0 bg-shark">Projects</h2>
+    <div id="projects" class="flex flex-col gap-10">
       {#each data.projects as project}
         <Project md={project} />
       {/each}
